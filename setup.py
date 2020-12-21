eb406fd1cd50ed77efe329137374e7fbeb986904
jumpingsquid <sv.sv82@gmail.com>
2020-12-21 18:40:00 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="ImageDB",
+    version="0.0.1",
+    author="Foresight",
+    author_email="sv.sv82@outlook.com",
+    packages=["ImageDB"],
+    description="A package for managing image with PostgresSQL",
+    url="https://github.com/JumpingSquid/ImageDB",
+    license='GPT',
+    python_requires='>=3.6',
+    install_requires=[
+    ]
+)
