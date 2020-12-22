f870ab8b3a5d002176511959264af4073880a8de
jumpingsquid <sv.sv82@gmail.com>
2020-12-22 16:36:19 +0800

    add: get_image(s) implementation

--- ImageDB/imagedb.py
+++ ImageDB/imagedb.py
@@ -1,6 +1,8 @@
 """
 imagedb is one of the core services of ImageDB, it is responsible for insert image metadata into
 PostgresSQL and retrieve image data.
+
+To avoid extensive I/O, imagedb in default return the filepath but not the actual image (perhaps in a np.array form).
 """
 
 import cv2
@@ -14,20 +16,30 @@ from ImageDB.connector import Connector
 
 class ImageDB:
 
-    def __init__(self, db_id, user_id):
+    def __init__(self, db_id: str, user_id: str, query_cache: bool):
         self.connector = Connector(db_id=db_id, user_id=user_id)
 
-    def add_imgtable(self, table_name):
+        if ~self.connector.isconnect():
+            self.connector.connect()
+
+        # query_cache function is to store the query result
+        # in the future, this might need to implement by using Memcache
+        # now it only stores in the memory and lose the information once restarted
+        self.query_cache_flag = query_cache
+        self.query_cache = {}
+
+    def add_dataset(self, dataset_id):
+        # dataset is a collection of image files
         cur = self.connector.get_cursor()
-        cur.execute(f"CREATE TABLE {table_name} (id serial PRIMARY KEY,"
+
+        # currently the schema of a dataset will store the information of
+        # image file with id, filepath, filename, and chksum
+        cur.execute(f"CREATE TABLE {dataset_id} (image_id serial PRIMARY KEY,"
                     f" filepath varchar , filename varchar, chksum varchar );")
 
     def add_image(self, filepath, checksum=False):
         # assert the file existence
         # default to close the checksum mechanism to avoid long processed time
-
-        if ~self.connector.isconnect():
-            self.connector.connect()
 
         assert os.path.isdir(filepath), "The file not exists"
 
@@ -48,10 +60,28 @@ class ImageDB:
         md5hash = hashlib.md5(Image.open(file_path).tobytes())
         return md5hash
 
-    def get_image(self):
-        cur = self.connector.get_cursor()
-        cur.execute("")
+    def get_image(self, dataset_id, image_id=None, file_name=None):
 
-    def get_images(self):
+        # if the cache function is turned on, then first find from the cache
+        if self.query_cache_flag and ((dataset_id, image_id, file_name) in self.query_cache):
+            return self.query_cache[(dataset_id, image_id, file_name)]
+
+        # get image based on id or filename from a dataset
+        assert image_id or file_name, 'Image id or the file name should provide at least one'
         cur = self.connector.get_cursor()
-        cur.execute("")
+        if image_id:
+            cur.execute(f"SELECT * FROM {dataset_id} WHERE image_id = {image_id}")
+        elif file_name:
+            cur.execute(f'SELECT * FROM {dataset_id} WHERE filename = {file_name}')
+
+        query_result = cur.fetchall()
+        if self.query_cache_flag:
+            self.query_cache[(dataset_id, image_id, file_name)] = query_result
+
+        return query_result
+
+    def get_images(self, dataset_id):
+        # get all the image form a dataset
+        cur = self.connector.get_cursor()
+        cur.execute(f"SELECT * FROM {dataset_id};")
+        return cur.fetchall()
