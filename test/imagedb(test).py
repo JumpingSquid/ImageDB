803ac1e21f0632815ef7c61cd116c3aaaafce95a
jumpingsquid <sv.sv82@gmail.com>
2020-12-23 15:37:50 +0800

    add: testcase

--- test/imagedb(test).py
+++ test/imagedb(test).py
@@ -7,10 +7,10 @@ class WidgetTestCase(unittest.TestCase):
     def setUp(self):
         self.image_db = ImageDB(db_id='test_db', user_id="test_account", password="test", query_cache=False)
 
-    def test_add_image(self):
+    def test_add_database(self):
         self.assertTrue(self.image_db.add_dataset('test'), 'fail to add the dataset')
 
-    def test_add_test(self):
+    def test_add_image(self):
         self.assertTrue(self.image_db.add_dataset('test'), 'fail to add the dataset')
         self.assertTrue(self.image_db.add_image('test',
                                                 'C:/Users/user/PycharmProjects/ImageDB/image_test/19741_en_1.jpg',
